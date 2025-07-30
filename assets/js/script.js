f58291314b36558e659131ed8437ef64f21bd018
Jairo Restrepo <jairo.restrepo@pcm.local>
2025-07-30 10:49:13 -0500

    Corrección final de about.html y sección OUR PEOPLE

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,25 @@
-fetch("components/navbar.html")
-  .then(res => res.text())
-  .then(data => {
-    document.getElementById("navbar-placeholder").innerHTML = data;
+// Cargar Navbar y activar pestaña actual
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarContainer = document.getElementById("navbar-placeholder");
 
-    setTimeout(() => {
-      const currentPage = window.location.pathname.split("/").pop() || "index.html";
-      const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+  fetch("components/navbar.html")
+    .then(res => res.text())
+    .then(html => {
+      navbarContainer.innerHTML = html;
 
-      navLinks.forEach(link => {
-        const linkPath = link.getAttribute("href").split("/").pop();
-        if (linkPath === currentPage) {
-          link.classList.add("active-link");
-        }
-      });
-    }, 0);
+      highlightActiveNav();
+    })
+    .catch(err => console.error("Error al cargar el navbar:", err));
+});
+
+function highlightActiveNav() {
+  const path = window.location.pathname.replace(/\/$/, '').split("/").pop() || "index.html";
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  navLinks.forEach(link => {
+    const href = link.getAttribute("href").replace(/\/$/, '').split("/").pop();
+    if (href === path) {
+      link.classList.add("active-link");
+    }
   });
+}
